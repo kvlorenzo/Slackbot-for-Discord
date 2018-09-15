25f0463f8a752a3bc05d39c04a7e02dc56cd69f6
kvlorenzo <kvlorenz@ucsd.edu>
2018-09-14 23:42:32 -0700

    added exception checking to Discord API importing
    Signed-off-by: kvlorenzo <kvlorenz@ucsd.edu>

--- slackbot.py
+++ slackbot.py
@@ -1,14 +1,21 @@
-from discord.ext import commands
+import sys
+
+try:
+    from discord.ext import commands
+except ImportError:
+    commands = None
+    print("Discord.py not installed on the computer.\n"
+          "Read the documentation on the website:\n"
+          "https://github.com/Rapptz/discord.py for installation instructions.")
+    sys.exit(1)
 
 token = "NDg4OTU4ODAxMDUyMTcyMjkw.Dnjyvg.Ys7qQ2N_Jk-brciE3j5y4ONKVVM"
 
 client = commands.Bot(command_prefix="/")
 
-
 @client.event
 async def on_ready():
     print("Bot connected")
     print("------")
-
 
 client.run(token)
