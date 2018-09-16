bab6d730198aaf68e441cfcb9a454c756c5cc1e7
kvlorenzo <kvlorenz@ucsd.edu>
2018-09-15 23:49:58 -0700

    changed print messages in onready function
    Signed-off-by: kvlorenzo <kvlorenz@ucsd.edu>

--- slackbot.py
+++ slackbot.py
@@ -1,8 +1,10 @@
 import sys
 
 try:
+    import discord
     from discord.ext import commands
 except ImportError:
+    discord = None
     commands = None
     print("Discord.py not installed on the computer.\n"
           "Read the documentation on the website:\n"
@@ -11,11 +13,18 @@ except ImportError:
 
 token = "NDg4OTU4ODAxMDUyMTcyMjkw.Dnjyvg.Ys7qQ2N_Jk-brciE3j5y4ONKVVM"
 
-client = commands.Bot(command_prefix="/")
+bot = commands.Bot(command_prefix="/")
 
-@client.event
+
+@bot.event
 async def on_ready():
     print("Bot connected")
     print("------")
+    print("Bot name:", bot.user.name)
+    print("Bot id:", bot.user.id)
+    print("Number of Servers:", len(bot.servers))
+    print("Number of Channels:", len(list(bot.get_all_channels())))
+    print("Number of Members:", len(list(bot.get_all_members())))
 
-client.run(token)
+
+bot.run(token)
