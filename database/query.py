958530c4681df1535e45c3814556e33954ab2f3f
kvlorenzo <kvlorenz@ucsd.edu>
2018-09-19 23:19:34 -0700

    added query.py
    Signed-off-by: kvlorenzo <kvlorenz@ucsd.edu>

---
+++ database/query.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+
+class Query:
+    def __init__(self, db):
+        self.conn = sqlite3.connect(db)
+        self.c = self.conn.cursor()
+
+    def get_member_id(self, member_dict):
+        self.c.execute("SELECT id from members WHERE server_id = :server_id "
+                       "AND channel_id = :channel_id AND user_id = :user_id",
+                       member_dict)
+        data = self.c.fetchall()
+        return data[0][0]
+
+    def close(self):
+        self.conn.commit()
+        self.conn.close()
